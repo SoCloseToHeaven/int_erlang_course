7c6557850f1885bb13c5b9cd88e593a9d889bb32
SoCloseToHeaven <halsofods@gmail.com>
2024-06-23 21:48:41 +0300

    add new block4 task - Mnesia DB + Sockets - Simple Key-Value db

---
+++ block4/db_client.erl
@@ -0,0 +1,21 @@
+%%%-------------------------------------------------------------------
+%%% @author dmitry
+%%% @copyright (C) 2024, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 23. июнь 2024 21:45
+%%%-------------------------------------------------------------------
+-module(db_client).
+-author("dmitry").
+
+%% API
+-export([
+  start_server/0,
+  stop_server/0
+]).
+
+
+start_server() -> ok.
+
+stop_server() -> ok.
