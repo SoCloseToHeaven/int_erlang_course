a98de2423739b59e9ffc98d84ad57af5a6c6fc04
SoCloseToHeaven <halsofods@gmail.com>
2024-06-26 21:51:40 +0300

    new handle_connection/0 func and execute_operation/1 that supports polish notation

--- block2/homework/calculator.erl
+++ block2/homework/calculator.erl
@@ -11,18 +11,14 @@
 
 -include_lib("kernel/include/logger.hrl").
 
+-define(ALLOWED_OPERATIONS, ['+', '-', '*', '/']).
+
 %% API
 -export([
   connect/0,
   handle_connection/0,
-  apply_operation/1,
-  add_operation/1,
-  add_operation/2,
   calculate/2,
-  sub_operation/1,
-  multiply_operation/1,
-  multiply_operation/2,
-  divide_operation/1
+  execute_operation/1
 ]).
 
 
@@ -30,25 +26,20 @@ connect() -> spawn(?MODULE, handle_connection, []).
 
 handle_connection() ->
   receive
-    {From, Message} when is_pid(From) -> From ! apply_operation(Message)
+    {From, Request} when is_pid(From) ->
+      Result = catch execute_operation(Request),
+      From ! Result
   end,
   handle_connection().
 
-apply_operation({Operation, Args} = Message) when is_atom(Operation) andalso is_list(Args) ->
-  case Message of {Atom, [Num, {NextOp, NextArgs} = Next]}
-    when is_atom(Atom) andalso is_number(Num) andalso is_atom(NextOp) andalso is_list(NextArgs) ->
-      case apply_operation(Next) of
-        {ok, Result} when is_number(Result) -> apply_operation({Atom, [Num, Result]});
-        {error, Reason} when is_atom(Reason) -> {error, Reason};
-        _Other -> {error, unsupported_behaviour}
-      end;
-    {add, Args} -> add_operation(Args);
-    {sub, Args} -> sub_operation(Args);
-    {mul, Args} -> multiply_operation(Args);
-    {'div', Args} -> divide_operation(Args);
-    _Other -> {error, no_such_operation}
-  end
-.
+
+execute_operation({Atom, [Num, {NextOp, NextArgs}]}) -> execute_operation({Atom, [Num] ++ [execute_operation({NextOp, NextArgs})]});
+execute_operation({Operation, Args}) ->
+  case is_allowed(Operation) of
+    true -> erlang:apply(erlang, Operation, Args);
+    _ -> throw(operation_not_allowed)
+  end;
+execute_operation(_) -> {error, bad_request}.
 
 calculate(Pid, {Operation, Args} = Message) when is_atom(Operation) andalso is_list(Args) ->
     From = self(),
@@ -58,27 +49,6 @@ calculate(Pid, {Operation, Args} = Message) when is_atom(Operation) andalso is_l
     end.
 
 
-add_operation([H | T]) when is_number(H) -> add_operation(H, T).
-add_operation(Acc, [H | T]) when is_number(Acc) andalso is_number(H)-> add_operation(Acc + H, T);
-add_operation(Acc, []) when is_number(Acc)-> {ok, Acc}.
-
-sub_operation([H | T]) -> {ok, H - lists:sum(T)}.
-
-multiply_operation(N) when is_number(N) -> multiply_operation([1 | [N]]);
-multiply_operation([H | T]) when is_number(H) -> {ok, multiply_operation(1, [H | T])}.
-multiply_operation(Acc, []) when is_number(Acc) -> Acc;
-multiply_operation(Acc, [H | T]) when is_number(Acc) andalso is_number(H) ->
-  NewAcc = Acc * H,
-  multiply_operation(NewAcc, T).
-
-
-divide_operation([H | T]) when is_number(H) ->
-  case multiply_operation(T) of
-    {ok, 0} -> {error, division_by_zero};
-    {ok, Result} when is_number(Result) ->
-      {ok, H / Result};
-    _Other -> {error, unsupported_mul_result}
-  end.
-
+is_allowed(Operation) -> lists:member(Operation, ?ALLOWED_OPERATIONS).
 
 
