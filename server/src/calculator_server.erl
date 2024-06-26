4afb68a8aa84a5d37722bd4aa0d1db01f150a367
SoCloseToHeaven <halsofods@gmail.com>
2024-06-26 20:54:47 +0300

    add simple operation executions and calculator_server calls

--- server/src/calculator_server.erl
+++ server/src/calculator_server.erl
@@ -13,11 +13,13 @@
 
 -record(state, {}).
 
+-define(ALLOWED_OPERATIONS, ['+', '-', '*', '/']).
+
 -behavior(gen_server).
 
 %% API
 -export([init/1, start_link/0, handle_call/3, handle_cast/2]).
--export([execute_operation/1]).
+-export([execute_operation/1, is_allowed/1]).
 
 
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
@@ -25,11 +27,18 @@ start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 init([]) -> {ok, #state{}}.
 
 
-handle_call({Op, Args}, _From, State) -> {reply, State};
+handle_call(Msg, _From, State) -> {reply, execute_operation(Msg), State};
 handle_call(Request, _From, State) -> {reply, {error, {unknown_request, Request}}, State}.
 
 handle_cast(_Request, State) -> {noreply, State}.
 
 
-execute_operation({Op, Args}) -> todo.
+execute_operation({Op, Args}) ->
+  case is_allowed(Op) of
+    true -> erlang:apply(erlang, Op, Args);
+    _ -> {error, bad_operation}
+  end;
+execute_operation(_) -> {error, bad_request}.
+
+is_allowed(Operation) -> lists:member(Operation, ?ALLOWED_OPERATIONS).
 
