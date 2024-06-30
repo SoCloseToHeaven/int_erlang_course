3d5db9128365b91c65e86c50b9473fb696388ebe
SoCloseToHeaven <halsofods@gmail.com>
2024-06-30 23:04:26 +0300

    block4 - new file tcp_server

--- block4/distributed_db_server/src/storage/storage.erl
+++ block4/distributed_db_server/src/storage/storage.erl
@@ -11,26 +11,9 @@
 
 -behavior(gen_server).
 
--record(config, {
-  table_info :: [{attributes, RecordInfo :: term()}],
-  schema_location :: term(),
-  debug :: none | verbose | debug | trace | false | true,
-  core_dir :: term(),
-  max_transfer_size :: term()
-}).
-
 
 -record(entry, {key :: term(), value :: term()}).
-
-
-
--define(DEFAULT_CONFIG, #config{
-  table_info = [{attributes, record_info(fields, entry)}],
-  schema_location = "~/distributed_db",
-  debug = true,
-  core_dir = "Mnesia",
-  max_transfer_size = 64000
-}).
+-record(state, {}).
 
 -define(COMMANDS, 'get' | 'set' | 'match' | 'delete').
 
@@ -42,9 +25,12 @@
 
 record_to_table_info(Record) when is_atom(Record) -> {attributes, record_info(fields, Record)}.
 
--spec init(Config :: #config{}) -> {ok, Pid :: pid()} | {error, Reason :: term()}.
-init(#config{table_info = Info, schema_location = SchemaLoc, debug = DebugFlag, core_dir = CoreDirName, max_transfer_size = MaxTransferSize})
-  -> erlang:error(not_implemented).
+-spec init([]) -> {ok, State :: #state{}} | {error, Reason :: term()}.
+init([]) ->
+  mnesia:create_schema([node()]),
+  mnesia:start(),
+  mnesia:create_table(entry, [{attributes, record_info(fields, entry)}]),
+  {ok, #state{}}.
 
 -spec process_command({Command :: ?COMMANDS, Args :: list()}) -> Result :: term() | {error, Reason :: term()}.
 process_command({Command, Args}) -> erlang:error(not_implemented).
@@ -53,5 +39,4 @@ process_command({Command, Args}) -> erlang:error(not_implemented).
 handle_call(Request, From, State) ->
   erlang:error(not_implemented).
 
-handle_cast(Request, State) ->
-  erlang:error(not_implemented).
+handle_cast(_Request, _State) -> erlang:error(not_implemented).
